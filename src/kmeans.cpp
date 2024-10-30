79d544bc8952ad4113409c7bb3cacfad80666fe1
Youthy WANG <youthywang@tamu.edu>
2024-10-30 14:54:11 -0500

    Correct some errors in vector type, 2024/10/30.

--- src/kmeans.cpp
+++ src/kmeans.cpp
@@ -20,28 +20,28 @@ arma::uvec MyKmeans_c(const arma::mat& X, int K,
     
     // Initialize any additional parameters if needed
     arma::vec n_ones = arma::ones(n);
-    arma::rowvec p_ones = arma::ones<arma::rowvec>(p);
+    arma::rowvec k_ones = arma::ones<arma::rowvec>(K);
     arma::vec X_norms = arma::sum(pow(X, 2), 1);
-    arma::vec M_norms = arma::sum(pow(M, 2), 1);
     arma::mat M_iter = M.t(); // copied centers (transpose of the origin)
     
     // For loop with kmeans algorithm
     for (int i = 0; i < numIter; i++){
       /* Iteration Step I: M-Step (Assignment)
        Preparations */
+      arma::rowvec M_norms = arma::sum(pow(M_iter, 2), 0);
       arma::mat XM = X * M_iter; // ij-th element: x_i^Tc_j
-      arma::mat dists = X_norms * p_ones + n_ones * M_iter - 2 * XM; // matrix between samples and centroids
+      arma::mat dists = X_norms * k_ones + n_ones * M_norms - 2 * XM; // matrix between samples and centroids
       //Compute and assign vectors to every current cluster
       Y = arma::index_min(dists, 1) + 1;
-      
+
       // Iteration Step II: E-Step (New Centers)
-      arma::mat M_temp = arma::zeros(p, K); // new centroids storage (temporary)
-      for (int j = 1; j <= K; j++){
-        arma::uvec index = arma::find(Y == j);
+      arma::mat M_temp(p, K, arma::fill::zeros); // new centroids storage (temporary)
+      for (int j = 0; j < K; j++){
+        arma::uvec index = arma::find(Y == j + 1);
         if (index.n_elem == 0) {
           throw std::runtime_error("ERROR: Bad Initial Centroids!");
         } else{
-          M_temp.col(j-1) = arma::mean(X.rows(index), 0).t();
+          M_temp.col(j) = arma::mean(X.rows(index), 0).t();
         }
       }
       if (arma::all(arma::vectorise(M_temp == M_iter))) {
