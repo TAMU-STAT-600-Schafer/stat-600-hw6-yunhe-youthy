ebc8940eade94525198aa5dab67fe3ac880e78fe
Yunhe Liu <yunhe.liu@tamu.edu>
2024-10-30 16:34:58 -0500

    Added some LR-wrapper function descriptions

--- R/LR_wrapper.R
+++ R/LR_wrapper.R
@@ -1,14 +1,16 @@
 
-#' Title
+#' Title Multi-class logistic regression Algorithm
 #'
-#' @param X 
-#' @param y 
-#' @param numIter 
-#' @param eta 
-#' @param lambda 
-#' @param beta_init 
+#' @param X A matrix with samples stored in rows (dim: \code{n * p}), 1st column should be 1s to account for intercept
+#' @param y A vector of size n of class labels, from 0 to K-1
+#' @param numIter Number of FIXED iterations of the algorithm, default value is 50
+#' @param eta Learning rate, default value is 0.1
+#' @param lambda Ridge parameter, default value is 1
+#' @param beta_init Initial starting values of beta for the algorithm, should be p x K matrix 
 #'
-#' @return
+#' @return a list includes the: beta - p x K matrix of estimated beta values after numIter iterations
+#' and objective - (numIter + 1) length vector of objective values of the function that we are minimizing at each iteration (+ starting value)
+#' 
 #' @export
 #'
 #' @examples
